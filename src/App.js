d656c15c6c2668ad2bc590fbb714ee7582729bf2
Amkam <amaduskamara@gmail.com>
2022-02-08 04:42:13 +0000

    Add a shuffle cards function and state for cards and turns

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 const cardImages = [
@@ -10,13 +11,25 @@ const cardImages = [
 ];
 
 function App() {
+  const [cards, setCards] = useState();
+  const [turns, setTurns] = useState(0);
 
-  
+  // Shuffle the cards
+  const shuffleCards = () => {
+    const shuffleCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffleCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
 
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
