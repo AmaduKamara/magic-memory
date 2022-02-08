e0214846830781cc28c8dbe3e94f47a408442641
Amkam <amaduskamara@gmail.com>
2022-02-08 04:29:51 +0000

    Add array of card images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import './App.css'
+import "./App.css";
+
+const cardImages = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
 
 function App() {
+
+  
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
@@ -9,4 +21,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
